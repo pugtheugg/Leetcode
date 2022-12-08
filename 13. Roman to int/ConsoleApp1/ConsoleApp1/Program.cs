b2ed4620dcd7e04425e077b720e2dba8f0dae839
pugtheugg <38682044+pugtheugg@users.noreply.github.com>
2022-12-08 14:24:30 +1000

    Completed task 13

---
+++ 13. Roman to int/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Solution {
+    public int RomanToInt(string s) {
+        var correspondingValue = new Dictionary<char, int>();
+		
+        correspondingValue.Add('I', 1);
+        correspondingValue.Add('V', 5);
+        correspondingValue.Add('X', 10);
+        correspondingValue.Add('L', 50);
+        correspondingValue.Add('C', 100);
+        correspondingValue.Add('D', 500);
+        correspondingValue.Add('M', 1000);
+		
+        var total = 0;
+		
+        for (var i = 0; i < s.Length; i++) 
+        {
+            var currentVal = correspondingValue[s[i]];
+			
+            if ((i + 1) < s.Length)
+            {
+                var nextVal = correspondingValue[s[i+1]];
+                if (currentVal < nextVal)
+                {
+                    total += (nextVal - currentVal);
+                    i += 1;
+                }
+                else
+                {
+                    total += currentVal;
+                }
+            }
+            else
+            {
+                total += currentVal;
+            }
+            Console.WriteLine(total);
+        }
+
+        return total;
+    }
+}
