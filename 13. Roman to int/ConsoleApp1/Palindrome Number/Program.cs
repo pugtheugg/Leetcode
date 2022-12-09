99e674ff1c4f26dc70ee728a05c8323d83158f3f
pugtheugg <38682044+pugtheugg@users.noreply.github.com>
2022-12-09 12:10:21 +1000

    Finished palindrome numbers question
    Finished the question and also changed the way that I will save questions now

---
+++ 13. Roman to int/ConsoleApp1/Palindrome Number/Program.cs
@@ -0,0 +1,13 @@
+﻿public class Solution {
+    public bool IsPalindrome(int x)
+    {
+        string xString = x.ToString();
+        
+        char[] charArray = xString.ToCharArray();
+        Array.Reverse(charArray);
+        
+        var reversedString = new string(charArray);;
+        
+        return reversedString == xString;
+    }
+}
