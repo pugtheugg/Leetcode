8297dc00ca38bb0f2fd405d2c9eb3b70e37543ed
pugtheugg <38682044+pugtheugg@users.noreply.github.com>
2022-12-11 15:29:05 +1000

    Did more questions
    Did two more questions one passed one fails due to execution time

---
+++ Solutions/Length of Last Word/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Solution {
+    public int LengthOfLastWord(string s)
+    {
+        string[] words = s.Split(' '); // Split the string into words 
+
+        var numOfLetters = 0;
+
+        for (var i = words.Length -1; i >= 0; i--) // Countdown for loop checks from last to first element of array
+        {
+            if (string.IsNullOrWhiteSpace(words[i])) continue; // If element is whitespace continue
+            numOfLetters = words[i].Length; // Otherwise get the length and exit the loop
+            break;
+        }
+        
+        return numOfLetters; // Return the value
+    }
+    
+    static void Main(string[] args)
+    {
+        // Display the number of command line arguments.
+        var s = "a";
+        var balls = new Solution().LengthOfLastWord(s);
+    }
+    
+}
---
+++ Solutions/Remove Duplicates from Sorted Array/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Solution {
+    public int RemoveDuplicates(int[] nums)
+    {
+        var len = nums.Length;
+        var k = 0;
+        
+        
+        for (var i = 0; i < len -1; i++)
+        {
+            if (nums[i] == 101) break; // All elements from here will be irrelevant 
+            
+            if (i != len && nums[i] == nums[i+1])
+            {
+                nums[i + 1] = 101; // Set duplicate to higher than max value 
+                i--; // Dont want to increment for loop as the array will be sorted
+                k++; // k counts the number of non unique elements 
+                Array.Sort(nums);
+            }
+        }
+
+        return len - k;
+    }
+}
+
+// Solution does not pass based on speed requirements
